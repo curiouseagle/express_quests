7927a9605855a861a16def7ffe00d611cb92abb0
curiouseagle <anais.maligebordes@gmail.com>
2022-12-12 11:51:20 +0100

    express quest 7 done

--- app.js
+++ app.js
@@ -26,7 +26,11 @@ const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
+// app.post("/api/users", userHandlers.postUser);
+
+const { hashPassword } = require("./auth.js");
+app.post("/api/users", hashPassword, userHandlers.postUser);
+
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
--- userHandlers.js
+++ userHandlers.js
@@ -2,7 +2,7 @@ const database = require("./database");
 
 
 const getUsers = (req, res) => {
-  let sql = "select * from users";
+  let sql = "select firstname, lastname, email, city, language from users";
   const sqlValues = [];
 
   if (req.query.language != null) {
@@ -35,7 +35,7 @@ const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select * from users where id = ?", [id])
+    .query("select firstname, lastname, email, city, language from users where id = ?", [id])
     .then(([users]) => {
       if (users[0] != null) {
         res.status(200).json(users[0]);
@@ -50,12 +50,12 @@ const getUserById = (req, res) => {
 };
 
 const postUser = (req, res) => {
-  const { firstname, lastname, email, city, language } = req.body;
+  const { firstname, lastname, email, city, language, hashedPassword } = req.body;
 
   database
     .query(
-      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-      [firstname, lastname, email, city, language]
+      "INSERT INTO users(firstname, lastname, email, city, language, hashedPassword) VALUES (?, ?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language, hashedPassword]
     )
     .then(([result]) => {
       res.location(`/api/users/${result.insertId}`).sendStatus(201);
